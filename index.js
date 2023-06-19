b2eeedb7031d0aa6749a3ec25ae3d0e7694c0aab
Samuel Maiko <sammymaiko721@gmail.com>
2023-06-19 21:40:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(record){
+    const result1=record.find(function(random){
+    return random.result==="W";
+    });
+
+    if(result1!== undefined){
+        return result1.year;
+    }
+    else{
+        return undefined;
+    }
+
+}
